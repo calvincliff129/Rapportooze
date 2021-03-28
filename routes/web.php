4117810a133c9d1ec3560d2ea251b1632c904ac1
Calvin Cliff <calvinnwoohaa1@gmail.com>
2021-03-29 00:48:12 +0800

    Created blade files for all web app features, added routes and controller for the contact page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
 Auth::routes();
 
@@ -26,4 +26,8 @@ Route::prefix('manage')->middleware('role:administrator')->group(function () {
 
 // Route::get('dashboard', 'App\Http\Controllers\UserController@dashboard')->middleware('auth');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Dashboard
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Contact
+Route::get('/contact', [App\Http\Controllers\ContactController::class, 'index'])->name('contact');
