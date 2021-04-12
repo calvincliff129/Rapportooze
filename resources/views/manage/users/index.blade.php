fc03d157b5cc309c74eaf49fdd4c14063d737ca2
calvincliff129 <calvinnwoohaa1@gmail.com>
2021-04-13 02:22:12 +0800

    Minor clean to the Manage User index code

--- resources/views/manage/users/index.blade.php
+++ resources/views/manage/users/index.blade.php
@@ -6,30 +6,30 @@
     <a href="{{route('users.create')}}" class="btn btn-secondary my-3"><i class="fa fa-user-plus me-1"></i> Create New User</a>
     <hr class="mt-0">
 
-        <div class="table-responsive">
-          <table class="table table-dark table-sm table-bordered table-hover table-striped text-center">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Date Created</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    <tr>
-                        <th class="text-center">{{$user->id}}</th>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>{{$user->created_at->toFormattedDateString()}}</td>
-                        <td class="has-text-right"><a class="btn btn-secondary me-4" href="{{route('users.show', $user->id)}}">View </a><a class="btn btn-light" href="{{route('users.edit', $user->id)}}">Edit</a></td>
-                    </tr>
-                @endforeach
-            </tbody>
-          </table>
-        </div>
+      <div class="table-responsive">
+        <table class="table table-dark table-sm table-bordered table-hover table-striped text-center">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Date Created</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+              @foreach ($users as $user)
+                  <tr>
+                      <th class="text-center">{{$user->id}}</th>
+                      <td>{{$user->name}}</td>
+                      <td>{{$user->email}}</td>
+                      <td>{{$user->created_at->toFormattedDateString()}}</td>
+                      <td class="has-text-right"><a class="btn btn-secondary me-4" href="{{route('users.show', $user->id)}}">View </a><a class="btn btn-light" href="{{route('users.edit', $user->id)}}">Edit</a></td>
+                  </tr>
+              @endforeach
+          </tbody>
+        </table>
+      </div>
 
     {{$users->links('vendor.pagination.bootstrap-4')}}
   </div>
