ff62820a8ee8afbc5f987b1dcd43ee7b8812ded6
calvincliff129 <calvinnwoohaa1@gmail.com>
2021-05-29 22:51:53 +0800

    Minor changes to sidebaruser

--- resources/views/layouts/navbars/sidebarUser.blade.php
+++ resources/views/layouts/navbars/sidebarUser.blade.php
@@ -7,13 +7,13 @@
             <li @if ($pageSlug == 'dashboard') class="active " @endif>
                 <a href="{{ route('home') }}">
                     <i class="fas fa-columns"></i>
-                    <p>{{ _('Dashboard') }}</p>
+                    <p>Dashboard</p>
                 </a>
             </li>
             <li @if ($pageSlug == 'contact') class="active " @endif>
                 <a href="{{ route('contact.index') }}">
                     <i class="far fa-user-circle"></i>
-                    <p>{{ _('Contacts') }}</p>
+                    <p>Contacts</p>
                 </a>
             </li>
             <!-- <li>
@@ -55,7 +55,7 @@
             <li @if ($pageSlug == 'profile') class="active " @endif>
                 <a href="{{ route('profile.edit') }}">
                     <i class="far fa-user"></i>
-                    <p>{{ _('User Profile') }}</p>
+                    <p>User Profile</p>
                 </a>
             </li>
         </ul>
