20e8ba1c10036ca9c1f57fbe86419fae99731cf3
calvincliff129 <calvinnwoohaa1@gmail.com>
2021-05-31 03:12:26 +0800

    Correction 15

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -349,7 +349,7 @@ class ContactController extends Controller
         
         $path = 'avatars';
         // if (Storage::disk('s3')->exists($path.'/'.$contact->avatar)) {
-            $url = Storage::disk('s3')->url($path.'/'.$contact->avatar);
+            $url = Storage::disk('s3')->url('rapportooze'.'/'.$path.'/'.$contact->avatar);
         // } else {
         //     $url = 0;
         // }
