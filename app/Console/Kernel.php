0d588817798cac04ab32a53cef15c54873f2eddf
calvincliff129 <calvinnwoohaa1@gmail.com>
2021-06-01 20:13:34 +0800

    Update 1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('reminder:emails')
-                ->everyTenMinutes();
-                // ->everyMinute();
+                // ->everyTenMinutes();
+                ->everyMinute();
                 // ->dailyAt('08:00');
     }
 
