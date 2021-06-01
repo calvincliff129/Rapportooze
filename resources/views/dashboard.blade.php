a9f67761644d160b3736bf25c8686bbffb67ff95
calvincliff129 <calvinnwoohaa1@gmail.com>
2021-06-01 20:36:34 +0800

    Update 6

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,27 +6,27 @@
         <div class="card" style="border-radius: 1rem;">
             <div class="card-body mb-3">
                 <div class="container text-center">
-                    <div class="row justify-content-center">
-                        <div class="col-2">
-                            <p class="card-title mb-4">Contacts</p>
+                    <div class="row">
+                        <div class="col-3">
+                            <h5 class="card-title mb-4">Contacts</h5>
                             <span class="bg-success border-0 rounded p-1 px-4">
                                 <strong>{{ $contactsCount }}</strong>
                             </span>
                         </div>
-                        <div class="col-2">
-                            <p class="card-title mb-4">Debts</p>
+                        <div class="col-3">
+                            <h5 class="card-title mb-4">Debts</h5>
                             <span class="bg-success border-0 rounded p-1 px-4">
                                 <strong>{{ $debtsCount }}</strong>
                             </span>
                         </div>
-                        <div class="col-2">
-                            <p class="card-title mb-4">Gifts</p>
+                        <div class="col-3">
+                            <h5 class="card-title mb-4">Gifts</h5>
                             <span class="bg-success border-0 rounded p-1 px-4">
                                 <strong>{{ $giftsCount }}</strong>
                             </span>
                         </div>
-                        <div class="col-2">
-                            <p class="card-title mb-4">Activities</p>
+                        <div class="col-3">
+                            <h5 class="card-title mb-4">Activities</h5>
                             <span class="bg-success border-0 rounded p-1 px-4">
                                 <strong>{{ $activitiesCount }}</strong>
                             </span>
